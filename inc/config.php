dbd972c945cd0257eb0862258584b5b1d14dd118
Scott Langdon <horsehead97@gmail.com>
2022-06-03 18:11:12 -0400

    api server changes

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST", "143.244.147.165");
-define("DB_USERNAME", 'freebsd');
-define("DB_PASSWORD", 'Bsd182690^0');
+define("DB_USERNAME", '');
+define("DB_PASSWORD", '');
 define("DB_DATABASE_NAME", 'srx_cart');
 
 ?>
